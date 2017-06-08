1109b1db846fd92142189890b13548296e0a4c56
Alexander Lifanov <lifanov.a.v@gmail.com>
2017-06-09 00:04:03 +0300

    add volume as parameter

--- prepare_time_series.py
+++ prepare_time_series.py
@@ -10,14 +10,22 @@ from keras.layers import Dense, LSTM, Activation
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
-look_back = 1
+from statsmodels.graphics.tsaplots import plot_acf
 
-df = pd.read_csv('eur_usd_15m.csv', usecols=[5])
-df_close = df['<CLOSE>']
+
+look_back = 1
+features = 2
+
+df = pd.read_csv('eur_usd_15m.csv', usecols=[5,6])
+df_close = df[['<CLOSE>', '<VOL>']]
 dataset = df_close.values[:300]
 dataset = dataset.astype('float32')
 
-dataset = np.expand_dims(dataset, axis=1)
+# plot autocorrelatio
+# plot_acf(dataset)
+# plt.show()
+
+# dataset = np.expand_dims(dataset, axis=1)
 
 # normalize the dataset
 scaler = MinMaxScaler(feature_range=(0, 1))
@@ -27,59 +35,61 @@ dataset = scaler.fit_transform(dataset)
 train_size = int(len(dataset) * 0.67)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+print(train.shape)
 
 
 def create_dataset(dataset, look_back=1):
     dataX, dataY = [], []
     for i in range(len(dataset) - look_back - 1):
-        a = dataset[i:(i + look_back), 0]
+        a = dataset[i:(i + look_back), :]
         dataX.append(a)
-        dataY.append(dataset[i + look_back, 0])
+        dataY.append(dataset[i + look_back, :])
     return np.array(dataX), np.array(dataY)
 
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
 
 # reshape input to be [samples, time steps, features]
-trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[2]))
+testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[2]))
+
 
 model = Sequential()
-model.add(LSTM(4, input_shape=(1, look_back)))
-model.add(Dense(1))
+model.add(LSTM(4, input_shape=(1, trainX.shape[2])))
+model.add(Dense(2))
 model.add(Activation('linear'))
 # model.add(Dense(64, input_dim=1, activation='relu'))
 # model.add(Dense(1))
 
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=0)
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
 
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
 # invert predictions
 trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform([trainY])
+trainY = scaler.inverse_transform(trainY)
 testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform([testY])
+testY = scaler.inverse_transform(testY)
 
 # calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+trainScore = math.sqrt(mean_squared_error(trainY[:, 0], trainPredict[:,0]))
 print('Train Score: %.6f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+testScore = math.sqrt(mean_squared_error(testY[:, 0], testPredict[:,0]))
 print('Test Score: %.6f RMSE' % (testScore))
 
 # shift train predictions for plotting
-trainPredictPlot = np.empty_like(dataset)
-trainPredictPlot[:, :] = np.nan
-trainPredictPlot[look_back:len(trainPredict) + look_back, :] = trainPredict
+trainPredictPlot = np.empty_like(dataset)[:, 0]
+trainPredictPlot[:] = np.nan
+trainPredictPlot[look_back:len(trainPredict) + look_back] = trainPredict[:, 0]
 
 # shift test predictions for plotting
-testPredictPlot = np.empty_like(dataset)
-testPredictPlot[:, :] = np.nan
-testPredictPlot[len(trainPredict) + (look_back * 2) + 1:len(dataset) - 1, :] = testPredict
+testPredictPlot = np.empty_like(dataset)[:, 0]
+testPredictPlot[:] = np.nan
+testPredictPlot[len(trainPredict) + (look_back * 2) + 1:len(dataset) - 1] = testPredict[:, 0]
 
 # plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
+plt.plot(scaler.inverse_transform(dataset)[:, 0])
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
 plt.show()
