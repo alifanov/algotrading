21855f0859968949327ed939d289cf676236fd32
Alexander Lifanov <lifanov.a.v@gmail.com>
2017-08-23 15:10:38 +0300

    add execution module

---
+++ mikasa_event_driven/execution.py
@@ -0,0 +1,56 @@
+import datetime
+import Queue
+
+from abc import ABCMeta, abstractmethod
+
+from .event import FillEvent, OrderEvent
+
+
+class ExecutionHandler(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def execute_order(self, event):
+        """
+        Takes an Order event and executes it, producing
+        a Fill event that gets placed onto the Events queue.
+
+        Parameters:
+        event - Contains an Event object with order information.
+        """
+        raise NotImplementedError("Should implement execute_order()")
+
+
+class SimulatedExecutionHandler(ExecutionHandler):
+    """
+    The simulated execution handler simply converts all order
+    objects into their equivalent fill objects automatically
+    without latency, slippage or fill-ratio issues.
+
+    This allows a straightforward "first go" test of any strategy,
+    before implementation with a more sophisticated execution
+    handler.
+    """
+
+    def __init__(self, events):
+        """
+        Initialises the handler, setting the event queues
+        up internally.
+
+        Parameters:
+        events - The Queue of Event objects.
+        """
+        self.events = events
+
+    def execute_order(self, event):
+        """
+        Simply converts Order objects into Fill objects naively,
+        i.e. without any latency, slippage or fill ratio problems.
+
+        Parameters:
+        event - Contains an Event object with order information.
+        """
+        if event.type == 'ORDER':
+            fill_event = FillEvent(datetime.datetime.utcnow(), event.symbol,
+                                   'BACKTEST', event.quantity, event.direction, None)
+            self.events.put(fill_event)
