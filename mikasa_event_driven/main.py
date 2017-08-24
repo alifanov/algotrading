830c16313969bafa1676c90c7049f1f1d1bb4012
Alexander Lifanov <lifanov.a.v@gmail.com>
2017-08-24 13:13:59 +0300

    add run() func

--- mikasa_event_driven/main.py
+++ mikasa_event_driven/main.py
@@ -1,40 +1,50 @@
 import time
 from Queue import Queue
 
-bars = DataHandler()
-strategy = Strategy()
-port = Portfolio()
-broker = ExecutionHandler()
+from data import HistoricCSVDataHandler
+from portfolio import NaivePortfolio
+from strategy import BuyAndHoldStrategy
+from execution import SimulatedExecutionHandler
 
 events = Queue(100)
 
-while True:
-    # Update the bars (specific backtest code, as opposed to live trading)
-    if bars.continue_backtest:
-        bars.update_bars()
-    else:
-        break
+broker = SimulatedExecutionHandler(events)
+bars = HistoricCSVDataHandler(events, '../datasets/', ['btc_etc'])
+strategy = BuyAndHoldStrategy(events, bars)
+port = NaivePortfolio(bars, events, None, 1000.0)
 
-    # Handle the events
+
+def run(heartbeat=3):
     while True:
-        try:
-            event = events.get(False)
-        except Queue.Empty:
-            break
+        # Update the bars (specific backtest code, as opposed to live trading)
+        if bars.continue_backtest:
+            bars.update_bars()
         else:
-            if event is not None:
-                if event.type == 'MARKET':
-                    strategy.calculate_signals(event)
-                    port.update_timeindex(event)
+            break
 
-                elif event.type == 'SIGNAL':
-                    port.update_signal(event)
+        # Handle the events
+        while True:
+            try:
+                event = events.get(False)
+            except Queue.Empty:
+                break
+            else:
+                if event is not None:
+                    if event.type == 'MARKET':
+                        strategy.calculate_signals(event)
+                        port.update_timeindex(event)
 
-                elif event.type == 'ORDER':
-                    broker.execute_order(event)
+                    elif event.type == 'SIGNAL':
+                        port.update_signal(event)
 
-                elif event.type == 'FILL':
-                    port.update_fill(event)
+                    elif event.type == 'ORDER':
+                        broker.execute_order(event)
 
-    # 3-sec heartbeat
-    time.sleep(3)
+                    elif event.type == 'FILL':
+                        port.update_fill(event)
+
+        # 3-sec heartbeat
+        time.sleep(heartbeat)
+
+if __name__ == "__main__":
+    run()
